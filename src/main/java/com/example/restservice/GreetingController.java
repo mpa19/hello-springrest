8aab550bf39c02da871e8f60373dd7b17721d570
Marc Perez <marcperezarnaiz@gmail.com>
2022-06-09 15:23:31 +0200

    Añadida nueva funcion en GrettingsController

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -12,6 +12,11 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+
+	@GetMapping("/")
+	public Greeting root()) {
+		return new Greeting(counter.incrementAndGet(), "Hola ke ase");
+	}
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
