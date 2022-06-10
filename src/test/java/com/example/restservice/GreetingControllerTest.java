5408674ec23e77f2cea30fe92e515514ee3ec6cd
Marc Perez <marcperezarnaiz@gmail.com>
2022-06-10 13:46:30 +0200

    Fixeado mocMvc error

--- src/test/java/com/example/restservice/GreetingControllerTest.java
+++ src/test/java/com/example/restservice/GreetingControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class GreetingControllerTest {
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private MockMvc mockMvc;
 
